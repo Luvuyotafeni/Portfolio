045d6def55c69c5ed6ec0f8cdff93e2cffd93f63
Luvuyotafeni <139848555+Luvuyotafeni@users.noreply.github.com>
2023-10-02 09:27:10 +0200

    changes

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+  function myFunction(){
+    window.open('https://classes.capaciti.org.za/courses/course-v1:CITI+CAP004+2021/courseware/b04db66d2e3a4dd88a95f19e9f8d5ada/10b419b882e84bffb0b4b025b0ace430/?activate_block_id=block-v1%3ACITI%2BCAP004%2B2021%2Btype%40sequential%2Bblock%4010b419b882e84bffb0b4b025b0ace430')
+  }
