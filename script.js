0e0ce64396d8a050f9f93b4116919812ed4b47d1
Luvuyotafeni <139848555+Luvuyotafeni@users.noreply.github.com>
2023-10-13 11:44:56 +0200

    changes

--- script.js
+++ script.js
@@ -8,3 +8,21 @@ function toggleMenu() {
    var open= window.open("about:blank","wrong password","width=200,height=200");
    open.document.write("wrong password");
   }
+
+   // Get the button
+   var scrollButton = document.getElementById("scrollButton");
+
+   // When the user scrolls down 20px from the top of the document, show the button
+   window.onscroll = function() {
+       if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+           scrollButton.style.display = "block";
+       } else {
+           scrollButton.style.display = "none";
+       }
+   };
+
+   // Scroll to the top of the document
+   function scrollToTop() {
+       document.body.scrollTop = 0; // For Safari
+       document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+   }
