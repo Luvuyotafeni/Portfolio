fe07602b0b1340735736c3ba2561ab614fae2843
Luvuyotafeni <139848555+Luvuyotafeni@users.noreply.github.com>
2023-10-26 13:14:42 +0200

    changes

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-  }
-  function myFunction(){
-   var open= window.open("about:blank","wrong password","width=200,height=200");
-   open.document.write("wrong password");
-  }
+
 
    // Get the button
    var scrollButton = document.getElementById("scrollButton");
